01a15d655c0657299a77ba6b7850f4c161bd9e24
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-09 09:11:45 -0400

    import loading component and render if state.loading true

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
